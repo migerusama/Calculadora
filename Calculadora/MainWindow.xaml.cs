a4ff034d9b45e6cd9b9e686fef9ac78900e6aa32
migerusama <migueldepablos99@gmail.com>
2021-11-30 12:38:37 +0100

    v3

--- Calculadora/MainWindow.xaml.cs
+++ Calculadora/MainWindow.xaml.cs
@@ -34,7 +34,6 @@ namespace Calculadora
             if (txtResultado.Text.Equals("0")) txtResultado.Text = "";
             else if (txtResultado.Text.Equals("-0")) txtResultado.Text = "-";
             txtResultado.Text += boton.Content;
-            //igual = false;
             HabilitarDeshabilitarBtn(true);
         }
 
@@ -43,19 +42,23 @@ namespace Calculadora
             var boton = (Button)sender;
 
             if (txtResultado.Text.Equals("Entrada no válida")) operacion += boton.Tag;
-            else operacion += txtResultado.Text + boton.Tag;
+            else
+            {
+                if(txtResultado.Text.Contains("E") || txtResultado.Text.Equals("∞") || txtResultado.Text.Contains(".") || txtResultado.Text.Contains(","))operacion += txtResultado.Text + boton.Tag;
+                else operacion += txtResultado.Text + ".00" + boton.Tag;
+            }
 
             txtResultado.Text = "0";
             txtOperacion.Text = operacion;
             operador = true;
             op = boton.Tag.ToString();
-            //igual = false;
         }
 
         private void BtnIgual_Click(object sender, RoutedEventArgs e)
         {
             try
             {
+                txtResultado.Text = txtResultado.Text.Replace(",", ".");
                 if (igual)
                 {
                     igual = txtResultado.Text.Equals(resultado);
@@ -74,45 +77,32 @@ namespace Calculadora
                     operacion += txtResultado.Text;
                     num = txtResultado.Text;
                 }
+                igual = true;
+                operador = false;
                 operacion = operacion.Replace(",", ".");
                 txtOperacion.Text = operacion;
-                /*Operacion necesaria para operar con numeros grandes o pequeños, sino da error por int32*/
-                operacion = Regex.Replace(operacion, @"\d+(\.\d+)?", m =>
+                if (!txtResultado.Text.Equals("∞") && !operacion.Contains("∞"))
                 {
-                    var x = m.ToString();
-                    return x.Contains(".") ? x : string.Format("{0}.0", x);
-                });
-                resultado = (Convert.ToDouble(dt.Compute(operacion, ""))).ToString("0.00");
-                txtHistorial.Text = txtOperacion.Text + "=" + txtResultado.Text + "\n\n" + txtHistorial.Text;
-                if (resultado.Substring(resultado.Length - 2, 2) == "00") resultado = resultado.Substring(0, resultado.Length - 3);
-                resultado = resultado.Replace(",", ".");
+                    resultado = (Convert.ToDouble(dt.Compute(operacion, ""))).ToString("0.00");
+                    txtHistorial.Text = txtOperacion.Text + "=" + txtResultado.Text + "\n\n" + txtHistorial.Text;
+                    if (!resultado.Equals("∞")) if (resultado.Substring(resultado.Length - 2, 2) == "00") resultado = resultado.Substring(0, resultado.Length - 3);
+                    resultado = resultado.Replace(",", ".");
+                }
+                else resultado = "∞";
                 txtResultado.Text = resultado;
                 operacion = "";
-                operador = false;
-                igual = true;
             }
             catch (DivideByZeroException)
             {
-                txtResultado.Text = "No se puede dividir por 0";
-                InterfazModoException();
+                InterfazModoException("No se puede dividir por 0", false);
             }
             catch (OverflowException)
             {
-                txtResultado.Text = "Numero muy grande";
-                InterfazModoException();
+                InterfazModoException("Numero muy grande", false);
             }
-
-
         }
 
-        private void InterfazModoException()
-        {
-            txtOperacion.Text = "";
-            HabilitarDeshabilitarBtn(false);
-            operacion = "";
-            operador = false;
-            igual = true;
-        }
+        
 
         private void BtnRaiz_Click(object sender, RoutedEventArgs e)
         {
@@ -122,24 +112,17 @@ namespace Calculadora
                 txtResultado.Text = "Entrada no válida";
             }
             else txtResultado.Text = Math.Sqrt(Convert.ToDouble(txtResultado.Text)).ToString();
-            //igual = false;
         }
 
         private void BtnElevar_Click(object sender, RoutedEventArgs e)
         {
             try
             {
-                operacion += txtResultado.Text;
-                txtResultado.Text = Math.Pow(Convert.ToDouble(operacion), 2).ToString();
-                //igual = false;
+                txtResultado.Text = Math.Pow(Convert.ToDouble(txtResultado.Text), 2).ToString();
             }
             catch (FormatException)
             {
-                txtResultado.Text = "Numero muy grande";
-                txtOperacion.Text = "";
-                HabilitarDeshabilitarBtn(false);
-                operacion = "";
-                operador = false;
+                InterfazModoException("Numero muy grande", false);
             }
 
         }
@@ -149,14 +132,12 @@ namespace Calculadora
             operacion = "";
             txtResultado.Text = "0";
             txtOperacion.Text = "";
-            //igual = false;
             HabilitarDeshabilitarBtn(true);
         }
 
         private void BtnCE_Click(object sender, RoutedEventArgs e)
         {
             txtResultado.Text = "0";
-            //igual = false;
             HabilitarDeshabilitarBtn(true);
         }
 
@@ -166,13 +147,11 @@ namespace Calculadora
             if (txtResultado.Text.Equals("Entrada no válida")) txtResultado.Text = "0";
             if (indice > 0) txtResultado.Text = txtResultado.Text.Remove(indice, 1);
             else txtResultado.Text = "0";
-            //igual = false;
         }
 
         private void BtnPorcentaje_Click(object sender, RoutedEventArgs e)
         {
             txtResultado.Text = (Convert.ToDouble(txtResultado.Text) / 100).ToString();
-            //igual = false;
         }
 
         private void BtnMasMenos_Click(object sender, RoutedEventArgs e)
@@ -180,38 +159,29 @@ namespace Calculadora
             if (txtResultado.Text.Equals("Entrada no válida")) txtResultado.Text = operacion;
             if (txtResultado.Text.Contains("-")) txtResultado.Text = txtResultado.Text.Remove(0, 1);
             else txtResultado.Text = "-" + txtResultado.Text;
-            //igual = false;
         }
 
         private void BtnPartido_Click(object sender, RoutedEventArgs e)
         {
             try
             {
+                if (txtResultado.Text == "0") throw new DivideByZeroException();
                 txtResultado.Text = (1 / Convert.ToDouble(txtResultado.Text)).ToString();
-                //igual = false;
             }
             catch (DivideByZeroException)
             {
-                txtResultado.Text = "Numero muy grande";
-                txtOperacion.Text = "";
-                HabilitarDeshabilitarBtn(false);
-                operacion = "";
-                operador = false;
-                igual = true;
+                InterfazModoException("No se puede dividir entre 0", false);
             }
         }
 
         private void BtnPunto_Click(object sender, RoutedEventArgs e)
         {
             if (!txtResultado.Text.Contains(".")) txtResultado.Text += ".";
-            //igual = false;
         }
+
         private void OnKeyDownHandler(object sender, KeyEventArgs e)
         {
-            if (e.Key == Key.Return)
-            {
-                BtnIgual_Click(sender, e);
-            }
+            if (e.Key == Key.Return) BtnIgual_Click(sender, e);
         }
 
         private void HabilitarDeshabilitarBtn(Boolean op)
@@ -246,6 +216,15 @@ namespace Calculadora
                 btnBorrar.IsEnabled = false;
                 btnIgual.IsEnabled = false;
             }
+        }
+
+        private void InterfazModoException(string message, bool boolean)
+        {
+            txtResultado.Text = message;
+            txtOperacion.Text = "";
+            HabilitarDeshabilitarBtn(boolean);
+            operacion = "";
+            operador = false;
         }
 
         private void BorrarHistorial(object sender, RoutedEventArgs e)
